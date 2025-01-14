87cf59abd1e367734ae08257ab4254ab6e615324
dhrypt <168616960+dhrypt@users.noreply.github.com>
2025-01-14 17:10:42 +0530

    Making IPC Typesafe

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,14 +1,14 @@
 import { app, BrowserWindow, ipcMain } from "electron";
 import path from "path";
-import { isDev } from "./util.js";
+import { ipcMainHandle, isDev } from "./util.js";
 import { getStaticData, pollResources } from "./resourceManager.js";
 import { getPreloadPath } from "./pathResolver.js";
 
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({
     webPreferences: {
-      preload: getPreloadPath()
-    }
+      preload: getPreloadPath(),
+    },
   });
   if (isDev()) {
     mainWindow.loadURL("http://localhost:5123");
@@ -18,7 +18,7 @@ app.on("ready", () => {
 
   pollResources(mainWindow);
 
-  ipcMain.handle("getStaticData", () => {
+  ipcMainHandle("getStaticData", () => {
     return getStaticData();
-  })
+  });
 });
