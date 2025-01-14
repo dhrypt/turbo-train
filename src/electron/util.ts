da42724896d375a68cf0b135b4329356904bce97
dhrypt <168616960+dhrypt@users.noreply.github.com>
2025-01-14 17:31:43 +0530

    Event Validation

--- src/electron/util.ts
+++ src/electron/util.ts
@@ -1,4 +1,6 @@
-import { ipcMain, WebContents } from "electron";
+import { ipcMain, WebContents, WebFrameMain } from "electron";
+import { pathToFileURL } from "url";
+import { getUIPath } from "./pathResolver.js";
 
 export function isDev(): boolean {
   return process.env.NODE_ENV === "development";
@@ -8,7 +10,14 @@ export function ipcMainHandle<Key extends keyof EventPayloadMapping>(
   key: Key,
   handler: () => EventPayloadMapping[Key]
 ) {
-  ipcMain.handle(key, () => handler());
+  ipcMain.handle(key, (event) => {
+    const frame = event.senderFrame;
+    if (!frame) {
+      throw new Error("Event sender frame is null");
+    }
+    validateEventFrame(frame);
+    return handler();
+  });
 }
 
 export function ipcWebContentsSend<Key extends keyof EventPayloadMapping>(
@@ -17,4 +26,14 @@ export function ipcWebContentsSend<Key extends keyof EventPayloadMapping>(
   payload: EventPayloadMapping[Key]
 ) {
   webContents.send(key, payload);
+}
+
+export function validateEventFrame(frame: WebFrameMain) {
+  console.log(frame.url);
+  if (isDev() && new URL(frame.url).host === "localhost:5123") {
+    return;
+  }
+  if (frame.url !== pathToFileURL(getUIPath()).toString()) {
+    throw new Error("Malicious event");
+  }
 }
